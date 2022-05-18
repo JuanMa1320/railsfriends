057b3ff2864b4120d965ea0eba8639a73563f4c1
JuanMa <jroaduran3@gmail.com>
2022-05-18 12:30:31 -0500

    user_id agrefado a friends controller y al formulario(_form.html.erb)

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -65,6 +65,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twiter)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twiter, :user_id)
     end
 end
